2a2418849f55032f3d885890b5087679fe2f36d8
David Simon <bdswebdev@gmail.com>
2014-09-25 18:03:22 +0100

    transactions except negative value, form except validation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 'use strict';
-var app = angular.module('wallet', ['ngStorage']);
+var app = angular.module('wallet', ['ngStorage', 'ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
+    $stateProvider
+        .state('home', {
+            url: "/",
+            templateUrl: "partials/home.html"
+        })
+        .state('state1.list', {
+            url: "/source",
+            templateUrl: "partials/source-view.html"
+        })
+});
 
 app.provider('currency', function () {
     return {
         $get: function () {
             return {
-                list:{
+                list: {
                     eur: {
                         code: "€",
                         class: "eur"
@@ -24,43 +37,59 @@ app.provider('currency', function () {
     }
 });
 
-
-app.controller('MenuCtrl', ['$scope', 'currency', '$localStorage', '$rootScope',
-    function ($scope, currency, $localStorage, $rootScope) {
-        $scope.$storage = $localStorage;
+app.controller('MenuCtrl', ['$scope', 'currency', '$localStorage', '$rootScope', '$state',
+    function ($scope, currency, $localStorage, $rootScope, $state) {
         $scope.$storage = $localStorage.$default({
             activeCurrency: "gbp",
-            transactions: [
-                {
-                    time: Date.now(),
-                    amount: 200,
-                    balance: 200
-                },
-                {
-                    time: Date.now(),
-                    amount: -100,
-                    balance: 100
-                }
-            ]
+            transactions: [],
+            balance: 0
         });
-        
+        $scope.reset = function () {
+            $scope.$storage.transactions = [];
+            $scope.$storage.balance = 0;
+            $state.go("home", null, { reload: true });
+        };
+
         $scope.currencies = currency;
-        console.log($scope.$storage.transactions);
-        
-        $scope.changeActive = function(currency){
+
+        $scope.changeActive = function (currency) {
             $scope.$storage.activeCurrency = currency;
-            $rootScope.$broadcast('UPDATE_CURRENCY',currency);
+            $rootScope.$broadcast('UPDATE_CURRENCY', currency);
         }
     }
 ]);
 
 app.controller('WalletCtrl', ['$scope', 'currency', '$localStorage', '$rootScope',
     function ($scope, currency, $localStorage, $rootScope) {
+        $scope.action = "add";
+        $scope.message ="Form";
         $scope.currencies = currency;
         $scope.$storage = $localStorage;
-        
+
         $scope.currency = $scope.currencies.list[$scope.$storage.activeCurrency];
-        $rootScope.$on('UPDATE_CURRENCY', function(event, newCurrency) {
+
+        $scope.save = function () {
+            var amount = parseInt($scope.amount);
+            if ($scope.action == "remove") {
+                amount = amount * -1;
+            }
+            var balance = parseInt($scope.$storage.balance) + amount;
+            if(balance > 0) {
+                $scope.$storage.transactions.push({
+                    time: Date.now(),
+                    amount: amount,
+                    balance: balance
+                });
+                $scope.$storage.balance = balance;
+                $scope.message = "Form";
+            }
+            else {
+                $scope.message = "Sorry but you cannot have a negative balance. The maximum amount you can remove from the wallet was placed into the input field.";
+                $scope.amount = $scope.$storage.balance;
+            }
+        }
+
+        $rootScope.$on('UPDATE_CURRENCY', function (event, newCurrency) {
             $scope.currency = $scope.currencies.list[newCurrency];
         });
     }
